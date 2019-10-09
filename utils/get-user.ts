9e5513f199e0a2a1a50587a3d9a136457c34e729
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-10-09 19:28:02 -0400

    fixed getUser

--- utils/get-user.ts
+++ utils/get-user.ts
@@ -3,12 +3,20 @@ import { getRepository } from "typeorm";
 import { User } from "../entities/user";
 
 export function getUser(token: string) {
-  const user: Partial<User> = jwt.verify(token, process.env.JWT_SECRET) as any;
-  return user;
+  try {
+    const user: Partial<User> = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    ) as any;
+    return user;
+  } catch (e) {
+    return null;
+  }
 }
 
 export async function getFullUser(token: string) {
   const decodedUser = getUser(token);
+  if (!decodedUser) return null;
   const userRepository = getRepository(User);
   const user = await userRepository.findOne(decodedUser.id);
   return user;
