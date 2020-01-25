13a014031ec2fe96b4cf76a1f6732ae3772a1d8e
Philip Van Raalte <phil.vanraalte@gmail.com>
2020-01-25 10:38:41 -0500

    added google maps

--- helpers.ts
+++ helpers.ts
@@ -1,5 +1,10 @@
 import { Column, ColumnOptions } from "typeorm";
+import { createClient } from "@google/maps";
 
 export function RelationColumn(options?: ColumnOptions) {
   return Column({ nullable: true, ...options });
 }
+
+export const client = createClient({
+  key: process.env.GOOGLE_API_KEY
+});
