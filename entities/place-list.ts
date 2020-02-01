4b3ef78df52e1a53d65d6c04f8bda8b918b5fa4b
Philip Van Raalte <phil.vanraalte@gmail.com>
2020-02-01 15:04:57 -0500

    removed imageUrl from PlaceList

--- entities/place-list.ts
+++ entities/place-list.ts
@@ -31,9 +31,4 @@ export class PlaceList {
     { cascade: ["insert"], eager: true }
   )
   places: Place[];
-
-  @Field(type => String)
-  get imageUrl(): string {
-    return this.places[0].imageUrl;
-  }
 }
